1001fc64a6f18c052b2ab1e57e824de5dd790005
Ashutosh Sharma <ashutoshsharma.120599@gmail.com>
2025-08-13 20:31:42 +0530

    Added user_id generate and anonymous_id generate when not found

--- jitsu-script.js
+++ jitsu-script.js
@@ -6,6 +6,26 @@
     return;
   }
 
+  function getCookie(name) {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      let cookie = cookies[i];
+      while (cookie.charAt(0) === " ") {
+        cookie = cookie.substring(1);
+      }
+      if (cookie.indexOf(name + "=") === 0) {
+        return cookie.substring(name.length + 1);
+      }
+    }
+    return null;
+  }
+
+  function setCookie(name, value) {
+    document.cookie = `${encodeURIComponent(name)}=${encodeURIComponent(
+      value || ""
+    )}; path=/`;
+  }
+
   function loadJitsu(pathname) {
     var prevScript = document.querySelector("script[data-jitsu]");
     if (prevScript) prevScript.remove();
@@ -17,10 +37,22 @@
     script.setAttribute("data-init-only", "true");
 
     var sessionId = sessionStorage.getItem("session_id") || null;
+    var userId = localStorage.getItem("user_id") || null;
+    var anonymousId = getCookie("__eventn_id");
 
     if (!sessionId) {
-      sessionId = crypto.randomUUID();
+      sessionId = `sess_id_${crypto.randomUUID()}`;
       sessionStorage.setItem("session_id", sessionId);
+    }
+
+    if (!userId) {
+      userId = `user_id_${crypto.randomUUID()}`;
+      localStorage.setItem("user_id", userId);
+    }
+
+    if (!anonymousId) {
+      anonymousId = crypto.randomUUID();
+      setCookie("__eventn_id", anonymousId);
     }
 
     script.onload = function () {
