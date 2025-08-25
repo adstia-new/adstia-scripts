a2bcc7f2e29452858a2b0c3f3a51464052e1f984
Ashutosh Sharma <ashutoshsharma.120599@gmail.com>
2025-08-25 21:04:07 +0530

    Added init function

--- adstia-scripts.js
+++ adstia-scripts.js
@@ -1,4 +1,61 @@
+const LOCAL_STORAGE_QUIZ_KEY = "quizValues";
+
+export const getDomainName = () => {
+  const location = window.location;
+
+  return location.host;
+};
+
+export const getCurrentSlug = () => {
+  const location = window.location;
+
+  return location.pathname;
+};
+
+export const getCurrentUrl = () => {
+  const location = window.location;
+
+  return location.href;
+};
+
+export const getScreenResolution = () => {
+  return `${window.screen.width}x${window.screen.height}`;
+};
+
+export const getConnectionType = () => {
+  const connection =
+    navigator.connection ||
+    navigator.mozConnection ||
+    navigator.webkitConnection;
+  return connection ? connection.effectiveType || "unknown" : "unknown";
+};
+
 window.adstiaScripts = {
+  init: function () {
+    // Save Extra Data to localStorage
+    const domainName = getDomainName();
+    const domainSlug = getCurrentSlug();
+    const finalUrl = getCurrentUrl();
+    const screenResolution = getScreenResolution();
+    const connectionType = getConnectionType();
+
+    const quizValues = localStorage.getItem(LOCAL_STORAGE_QUIZ_KEY);
+    const parsedQuizValues = quizValues ? JSON.parse(quizValues) : {};
+
+    const newQuizValuesData = {
+      ...parsedQuizValues,
+      domainName,
+      domainSlug,
+      finalUrl,
+      screenResolution,
+      connectionType,
+    };
+
+    localStorage.setItem(
+      LOCAL_STORAGE_QUIZ_KEY,
+      JSON.stringify(newQuizValuesData)
+    );
+  },
   replaceShortcodes: function (str) {
     try {
       if (typeof window !== "undefined") {
