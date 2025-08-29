d1220f882804ba21f1c8dc27a0a4647596eef06d
Ashutosh Sharma <ashutoshsharma.120599@gmail.com>
2025-08-29 23:53:00 +0530

    Updated adstiaScripts to include fetchUserLocationAndDeviceInfo function

--- adstia-scripts.js
+++ adstia-scripts.js
@@ -32,7 +32,6 @@ const getConnectionType = () => {
 
 window.adstiaScripts = {
   init: function () {
-    // Save Extra Data to localStorage
     const domainName = getDomainName();
     const domainSlug = getCurrentSlug();
     const finalUrl = getCurrentUrl();
@@ -56,6 +55,81 @@ window.adstiaScripts = {
       JSON.stringify(newQuizValuesData)
     );
   },
+
+  fetchUserLocationAndDeviceInfo: async function () {
+    if (
+      typeof window === "undefined" ||
+      !window?.cf_variable?.IP_ADDRESS_API_KEY
+    )
+      return;
+
+    const IP_ADDRESS_API_KEY = window.cf_variable?.IP_ADDRESS_API_KEY;
+    const IP_ADDRESS_API_URL = "https://api.ipify.org?format=json";
+    const IP_ADDRESS_BASED_LOCATION_API_URL =
+      "https://server.adstiacms.com/api/ip-address";
+    const LOCAL_STORAGE_QUIZ_KEY = "quizValues";
+
+    try {
+      if (typeof window !== "undefined") {
+        const storedData = JSON.parse(
+          localStorage.getItem(LOCAL_STORAGE_QUIZ_KEY) || "{}"
+        );
+
+        if (!storedData.websiteZip) {
+          const ipResponse = await fetch(IP_ADDRESS_API_URL);
+          const ipData = await ipResponse.json();
+          const userIp = ipData.ip;
+
+          const locationResponse = await fetch(
+            IP_ADDRESS_BASED_LOCATION_API_URL,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                ip: userIp,
+                key: IP_ADDRESS_API_KEY,
+              }),
+            }
+          );
+
+          if (!locationResponse.ok) {
+            throw new Error(`HTTP error! status: ${locationResponse.status}`);
+          }
+
+          const data = await locationResponse.json();
+          const uaResult = UAParser();
+          const savedTags = JSON.parse(
+            localStorage.getItem(LOCAL_STORAGE_QUIZ_KEY) || "{}"
+          );
+
+          localStorage.setItem(
+            LOCAL_STORAGE_QUIZ_KEY,
+            JSON.stringify({
+              ipCity: data.data.city,
+              ipCountry: data.data.country,
+              ipState: data.data.state,
+              ipZip: data.data.postalCode,
+              ipAddress: data.data.ipAddress,
+              ipCountryCode: data.data.countryCode,
+              device: uaResult.device.model?.toString() || "",
+              browser: uaResult.browser.name?.toString() || "",
+              os: uaResult.os.name?.toString() || "",
+              userAgent: uaResult.ua?.toString() || "",
+              osVersion: uaResult.os.version?.toString() || "",
+              browserVersion: uaResult.browser.version?.toString() || "",
+              deviceModel: uaResult.device.model?.toString() || "",
+              ...savedTags,
+            })
+          );
+        }
+      }
+    } catch (error) {
+      console.error("Fetch error:", error);
+    }
+  },
+
   replaceShortcodes: function (str) {
     try {
       if (typeof window !== "undefined") {
