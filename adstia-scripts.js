dda617f241df926dfff370e79ffff47dccd5dfbf
Ashutosh Sharma <ashutoshsharma.120599@gmail.com>
2025-09-09 22:42:07 +0530

    Rollback replaceShortcodes function

--- adstia-scripts.js
+++ adstia-scripts.js
@@ -176,27 +176,31 @@ window.adstiaScripts = {
 
   replaceShortcodes: function (str) {
     try {
-      const urlSearchParams = new URLSearchParams(window.location.search);
+      if (typeof window !== "undefined") {
+        const urlSearchParams = new URLSearchParams(window.location.search);
 
-      const paramsShortcodes = Object.fromEntries(urlSearchParams.entries());
+        const paramsShortcodes = Object.fromEntries(urlSearchParams.entries());
 
-      const userId = localStorage.getItem("user_id") || "";
+        const userId = localStorage.getItem("user_id") || "";
 
-      const shortcodes = {
-        user_id: userId,
-        referral_d: window.location.hostname,
-        ...paramsShortcodes,
-      };
+        const shortcodes = {
+          user_id: userId,
+          referral_d: window.location.hostname,
+          ...paramsShortcodes,
+        };
 
-      const replaced = str.replace(/\{\{query\.(.*?)\}\}/g, (_, match) => {
-        return shortcodes[match.trim()] || "";
-      });
+        const replaced = str.replace(/\{\{query\.(.*?)\}\}/g, (_, match) => {
+          return shortcodes[match.trim()] || "";
+        });
 
-      if (replaced.endsWith("?")) {
-        return replaced.slice(0, -1);
+        if (replaced.endsWith("?")) {
+          return replaced.slice(0, -1);
+        }
+
+        return replaced;
       }
-
-      return replaced;
+      
+      return "";
     } catch (e) {
       console.error("Failed to replace Shortcodes", e);
       return str;
